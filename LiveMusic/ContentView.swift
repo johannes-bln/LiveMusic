17a7fcae6e34947c63ddd98a1b140823e7ff48dc
Johannes <optik.kavallerie.0z@icloud.com>
2025-10-30 20:19:30 +0100

    Initial Upload
    Xcode project created and files uploaded to GitHub for launch of coding.

---
+++ LiveMusic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LiveMusic
+//
+//  Created by Johannes Glückler on 30.10.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LiveMusic/LiveMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveMusicApp.swift
+//  LiveMusic
+//
+//  Created by Johannes Glückler on 30.10.25.
+//
+
+import SwiftUI
+
+@main
+struct LiveMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
