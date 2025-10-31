fb8955373e12438f789c32576f173f2638a9f40e
Johannes <me@johannes-bln.de>
2025-10-31 20:37:25 +0100

    Add AlbumCoverView (WIP Version)

--- LiveMusic/Views/Shared/AlbumCoverView.swift
+++ LiveMusic/Views/Shared/AlbumCoverView.swift
@@ -7,3 +7,52 @@
 
 import Foundation
 import SwiftUI
+
+struct AlbumView: View {
+    let musicItem: ShazamMusicItem
+    @ViewBuilder
+    var body: some View {
+
+        if let imageURL = musicItem.appleMusicURL {
+            AsyncImage(
+                url: imageURL,
+                transaction: Transaction(animation: .easeInOut)
+            ) { phase in
+                switch phase {
+                case .empty:
+                    placeholder
+                case .success(let image):
+                    image
+                        .resizable()
+                        .scaledToFill()
+                        .transition(.opacity)
+                case .failure:
+                    errorView
+                @unknown default:
+                    errorView
+                }
+            }
+        }
+    }
+
+    private var placeholder: some View {
+        ZStack {
+            Color.secondary.opacity(0.1)
+            Image(systemName: "music.note")
+                .imageScale(.large)
+                .foregroundStyle(.secondary)
+        }
+    }
+
+    private var errorView: some View {
+        ZStack {
+            Color.secondary.opacity(0.1)
+            Image(systemName: "exclamationmark.triangle")
+                .foregroundStyle(.secondary)
+        }
+    }
+}
+
+#Preview {
+    AlbumView(musicItem: exampleShazamMusicItem)
+}
